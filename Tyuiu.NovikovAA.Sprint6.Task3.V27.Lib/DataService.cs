989d282f8b52f0b06d963e96d5992774fa5cd4dc
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-18 21:00:07 +0600

    Task3

---
+++ Tyuiu.NovikovAA.Sprint6.Task3.V27.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+
+namespace Tyuiu.NovikovAA.Sprint6.Task3.V27.Lib
+{
+    public class DataService : ISprint6Task3V27
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1; // Количество строк
+            int columns = matrix.Length / rows;     // Количество столбцов
+
+            // Создаем новую матрицу для хранения результата
+            int[,] result = new int[rows, columns];
+
+            // Копируем данные из исходной матрицы в новую
+            Array.Copy(matrix, result, matrix.Length);
+
+            // Сортируем строки по значению в четвёртом столбце
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = i + 1; j < rows; j++)
+                {
+                    if (result[i, 3] > result[j, 3])
+                    {
+                        // Меняем строки местами
+                        for (int k = 0; k < columns; k++)
+                        {
+                            int temp = result[i, k];
+                            result[i, k] = result[j, k];
+                            result[j, k] = temp;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.NovikovAA.Sprint6.Task3.V27.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+
+using Tyuiu.NovikovAA.Sprint6.Task3.V27.Lib;
+
+
+namespace Tyuiu.NovikovAA.Sprint6.Task3.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
